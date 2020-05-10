fb869ffe08be39656e29118922522875b3e9a319
Artyom <artyomnurgaliev>
2020-05-10 16:17:50 +0300

    added IR-tree realization

---
+++ irtree-optimization/visitors/TemplateVisitor.cpp
@@ -0,0 +1,13 @@
+#include <irtree/tree_wrapper/SubtreeWrapper.h>
+#include <objects/objs/Object.h>
+#include "TemplateVisitor.h"
+#include "elements.h"
+
+template<typename T>
+T TemplateVisitor<T>::Accept(BaseElement * element) {
+    element->Accept(this);
+    return tos_value_;
+}
+
+template Object* TemplateVisitor<Object*>::Accept(BaseElement* element);
+template IRT::SubtreeWrapper* TemplateVisitor<IRT::SubtreeWrapper*>::Accept(BaseElement *element);
